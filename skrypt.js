c3599e1c27eec7977df1d46ae707fd0f9703107d
Daniel Romanowski <daniel@dotlink.pl>
2022-05-12 08:41:12 +0200

    formatowanie kodu

--- skrypt.js
+++ skrypt.js
@@ -1,6 +1,6 @@
-function data()
-{
-let d = new Date();
-document.body.innerHTML = "<h1>Ta strona została odwiedzona <br> " + d + "</h1>"
+function data() {
+	let d = new Date();
+	document.body.innerHTML =
+		'<h1>Ta strona została odwiedzona <br> ' + d + '</h1>';
 }
 setInterval(data, 1000);
